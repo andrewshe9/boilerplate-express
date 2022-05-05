d7fce645e00d1d656e837560d114a92752fe1c32
AiqinZ <andrew.sheh@hotmail.com>
2022-05-05 16:27:38 +1000

    add static assets

--- myApp.js
+++ myApp.js
@@ -3,9 +3,10 @@ let app = express();
 console.log("Hello World")
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname+/views/index.html);
+  res.sendFile(__dirname+"/views/index.html");
 });
-
+ 
+app.use(__dirname + "/public", express.static());
 
 
 
