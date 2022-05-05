e8f5866f4fa478628b510e56cb7feafaac75c144
AiqinZ <andrew.sheh@hotmail.com>
2022-05-05 17:44:08 +1000

    env variable 1

--- myApp.js
+++ myApp.js
@@ -1,46 +1,17 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-console.log("Hello World")
+console.log("Hello World");
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname+"/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
- 
+
 app.use("/public", express.static(__dirname + "/public"));
 
-
 app.get("/json", function (req, res) {
-  res.json({ "message": "Hello json" });
+  process.env.MESSAGE_STYLE === "uppercase"
+    ? res.json({ message: "HELLO JSON" })
+    : res.json({ message: "Hello json" });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
